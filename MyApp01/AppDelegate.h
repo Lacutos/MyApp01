feb84fa5c1ba959219793b7f811abc56aa21fa20
Lacutos <menshiid@gmail.com>
2013-06-14 12:01:08 +0800

    Initial Commit

---
+++ MyApp01/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  MyApp01
+//
+//  Created by Lacutos on 13/6/14.
+//  Copyright (c) 2013年 Jason.Cai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
